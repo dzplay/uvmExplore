999208785e77855744195304a7d5877252c04b36
dzplay <dzplay@qq.com>
2024-08-03 13:02:24 +0800

    update scripts

--- common/wheels/wheels.sv
+++ common/wheels/wheels.sv
@@ -13,17 +13,22 @@ typedef enum bit {true=0, false=1} bool_e;
 
 class KeyValue extends uvm_object;
 
-  string  key;
-  int     value;
+    string  key;
+    int     value_int  = -1; // default invald
+    long    value_long = -1;
+    string  value_str  = ""; 
 
-   `uvm_object_utils_begin(KeyValue)
-     `uvm_field_string(key, UVM_ALL_ON | UVM_NOPACK);
-     `uvm_field_int(value, UVM_ALL_ON | UVM_NOPACK);
-   `uvm_object_utils_end
+    `uvm_object_utils_begin(KeyValue)
+        `uvm_field_string(key, UVM_ALL_ON | UVM_NOPACK);
+        `uvm_field_int(value, UVM_ALL_ON | UVM_NOPACK);
+        `uvm_field_long(value, UVM_ALL_ON | UVM_NOPACK);
+        `uvm_field_string(value_str, UVM_ALL_ON | UVM_NOPACK);
+    `uvm_object_utils_end
 
-  function new(string name="KeyValue");
-     super.new(name);
-  endfunction
+    function new(string name="KeyValue");
+        super.new(name);
+        key = name;
+    endfunction
 
 endclass: KeyValue
 
